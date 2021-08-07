fa4ec533705db276249bb339bdb8a12f3eeca18c
RMOATMAN <raemarie.oatman@gmail.com>
2021-08-07 10:12:00 -0400

    added file pathname

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-Routes');
+const homeRoutes = require('./home-Routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
